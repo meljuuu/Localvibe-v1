2d59b964cdc84b70553771f326f0f3448cacad9e
porarisuu <112392424+PolaDasma123@users.noreply.github.com>
2024-11-30 15:07:25 +0800

    Update report.js

--- server/routes/report.js
+++ server/routes/report.js
@@ -13,7 +13,7 @@ const router = express.Router();
 router.route("/create-or-update-report").post(isAuthenticatedUser, createOrUpdateReport);
 router.route("/get-all-reports").get(isAuthenticatedUser, getAllReports);
 router.route("/get-all-reports:reportedItemId/:itemType").get(isAuthenticatedUser, getReportsByItem);
-router.route("/get-all-reports/:id").delete(isAuthenticatedUser, deleteReport);
-router.route("/get-reports-by-item/:reportedItemId/:itemType").delete(isAuthenticatedUser, deleteReportsByItem);
+router.route("/delete-report/:id").delete(isAuthenticatedUser, deleteReport);
+router.route("/delete-reports-by-item/:reportedItemId/:itemType").delete(isAuthenticatedUser, deleteReportsByItem);
 
 module.exports = router;
