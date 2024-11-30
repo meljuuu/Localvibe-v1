96a6c1563565ed7a573453505e15c991a7a6b8df
porarisuu <112392424+PolaDasma123@users.noreply.github.com>
2024-11-30 17:07:31 +0800

    Update pin.js

--- server/routes/pin.js
+++ server/routes/pin.js
@@ -5,29 +5,30 @@ const {
   getPinById,
   updatePinById,
   deletePinById,
-  deleteReview,
-  modifyReview,
   addReview,
+  modifyReview,
+  deleteReview,
+  incrementVisitCount,  // Import the incrementVisitCount controller
 } = require("../controllers/pin");
 
 const { isAuthenticatedUser } = require("../middleware/auth");
 
 const router = express.Router();
 
+// Pin routes
 router.route("/create-pin").post(isAuthenticatedUser, createPin);
-
 router.route("/get-all-pins").get(isAuthenticatedUser, getAllPins);
-
 router.route("/get-pin/:id").get(isAuthenticatedUser, getPinById);
-
 router.route("/update-pin/:id").put(isAuthenticatedUser, updatePinById);
-
 router.route("/delete-pin/:id").delete(isAuthenticatedUser, deletePinById);
 
+// Review routes
 router.route("/add-review").post(isAuthenticatedUser, addReview);
-
 router.route("/modify-review").put(isAuthenticatedUser, modifyReview);
-
 router.route("/delete-review").delete(isAuthenticatedUser, deleteReview);
 
+// Visit count route
+router.route("/increment-visit-count").post(isAuthenticatedUser, incrementVisitCount);
+
 module.exports = router;
+  
