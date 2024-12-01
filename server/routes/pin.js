ed8d309311ad987b2b3c9b6ed3db7da0c203ce05
Jami <147174476+VenJami@users.noreply.github.com>
2024-12-01 14:19:36 +0800

    Update pin.js

--- server/routes/pin.js
+++ server/routes/pin.js
@@ -11,6 +11,7 @@ const {
   incrementVisitCount,  // Import the incrementVisitCount controller
 } = require("../controllers/pin");
 
+
 const { isAuthenticatedUser } = require("../middleware/auth");
 
 const router = express.Router();
