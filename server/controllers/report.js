e606e087bddb197edca73eee681ec45c4fd34026
porarisuu <112392424+PolaDasma123@users.noreply.github.com>
2025-02-17 22:21:40 +0800

    Update report.js

--- server/controllers/report.js
+++ server/controllers/report.js
@@ -5,8 +5,7 @@ const catchAsyncErrors = require("../middleware/catchAsyncErrors");
 // Create or increment a report
 exports.createOrUpdateReport = catchAsyncErrors(async (req, res, next) => {
   try {
-    const { userId, reportedItemId, itemType, reason, itemTitle, imageUrl } = req.body;
-
+    const { userId, reportedItemId, itemType, reason, itemTitle, imageUrl } = req.body
     // Validate required fields
     if (!userId || !reportedItemId || !itemType || !reason || !itemTitle || !imageUrl) {
       return next(new ErrorHandler("All fields are required", 400));
