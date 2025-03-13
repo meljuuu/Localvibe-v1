1311d1bcfc3ef0da09b74aa88b5da55d324027e3
meljuuu <polarisjumel@gmail.com>
2025-03-14 01:35:25 +0800

    123

--- client/redux/URI.tsx
+++ client/redux/URI.tsx
@@ -3,9 +3,9 @@ import {Platform} from 'react-native';
 let URI = '';
 
 if (Platform.OS === 'ios') {
-  URI = 'https://localvibe.vercel.app/api/v1';
+  URI = 'https://localvibe-delta.vercel.app/api/v1';
 } else {
-  URI = 'https://localvibe.vercel.app/api/v1';
+  URI = 'https://localvibe-delta.vercel.app/api/v1';
 }
 
 export {URI};
